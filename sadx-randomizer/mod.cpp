187516ec3a13bd8e2fe44fb7d5dab22b00972b2c
Sora <sora.x@hotmail.fr>
2019-05-30 11:15:48 +0200

    Fixing

--- sadx-randomizer/mod.cpp
+++ sadx-randomizer/mod.cpp
@@ -15,6 +15,7 @@ bool Regular = false;
 
 extern "C"
 {
+	
 
 	time_t t;
 
@@ -31,7 +32,7 @@ extern "C"
 		seed = config->getInt("Randomizer", "Seed", 0);
 		Regular = config->getBool("Randomizer", "Regular", false);
 		delete config;
-
+	
 		if (seed)
 			srand(seed);
 		else
@@ -39,17 +40,15 @@ extern "C"
 
 		if (RNGStages == true || RNGCharacters == true)
 		{
-			WriteCall((void*)0x416bd4, quitstage); //hook pause quit
-			WriteCall((void*)0x41509b, randomstage);  //Hook "SetNextLevel" Used after some specific boss fight? Seems like it fixs egg viper crash.
-			WriteCall((void*)0x7b0b0e, randomstage); //hook "Set next level"
-			WriteCall((void*)0x416bf8, quitstage); //hook "Set next level cutscene version" When used, Big Red Mountain and WV are fixed.
-			WriteCall((void*)0x41709d, randomstage); //hook "Go to next level"
+			WriteCall((void*)0x41709d, randomstage); //hook after finishing a stage
 			WriteCall((void*)0x50659a, randomstage); //hook trial mod / hedgehog hammer / sub game
-			WriteCall((void*)0x417b47, randomstage); //hook when entering to an action stage in the hub world. MIGHT BE AN ISSUE
+			WriteCall((void*)0x417b47, randomstage); //hook when entering to an action stage in the hub world.
 			WriteCall((void*)0x42ca8c, randomstage); //hook when selecting a character in adventure mode.
-			WriteCall((void*)0x41342a, randomstage); //hook CurrentAdventureData (If hook is disabled, you will get randomly teleported to Gamma's upgrade room for some reason.)
-			WriteCall((void*)0x413522, randomstage); //hook CurrentAdventureData Boss soft reset (Same as before, happen if you soft reset during a boss fight.)
+			WriteCall((void*)0x41342a, randomstage); //hook CurrentAdventureData
+			WriteCall((void*)0x413522, randomstage); //hook CurrentAdventureData Boss soft reset
 		}
+		
+
 	}
 
 	__declspec(dllexport) void __cdecl OnFrame()
@@ -72,173 +71,46 @@ extern "C"
 			EventFlagArray[EventFlags_Gamma_LaserBlaster] = true;
 		}
 
-		
-
-		//When loading: check if Credits need to start and call random act if possible.
-
 		DataPointer(char, Emblem, 0x974AE0);
 		DataPointer(unsigned char, LevelList, 0x3B2C5F8);
 		DataPointer(unsigned char, SelectedCharacter, 0x3B2A2FD);
-		int actrng[2] = { 0, 1 };
-		int actHS[2] = { 0, 2 };
-		int actIC[2] = { 0, 3 };
 
 		if (GameState == 21 && (GameMode == 5 || GameMode == 4 || GameMode == 17 && (LevelList == 0 || LevelList == 97 || LevelList == 243)))
 		{
-			if (Emblem == 10 || Emblem == 16 || Emblem == 22 || Emblem == 26 || Emblem == 31 || Emblem == 37 || Emblem == 39)
+			if (Emblem == 10 || Emblem == 15 || Emblem == 20 || Emblem == 23 || Emblem == 27 || Emblem == 32 || Emblem == 33) 
 			{
-				// Check if credits need to happen, if not, start the RNG to get Metal Sonic and a random act.
-				switch (SelectedCharacter)
-				{
-				case 0:
-					EventFlagArray[EventFlags_SonicAdventureComplete] = true;
-					EventFlagArray[EventFlags_TailsUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_KnucklesUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_AmyUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_BigUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_GammaUnlockedAdventure] = true;
-					break;
-				case 1:
-					EventFlagArray[EventFlags_TailsAdventureComplete] = true;
-					EventFlagArray[EventFlags_KnucklesUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_AmyUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_BigUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_GammaUnlockedAdventure] = true;
-					break;
-				case 2:
-					EventFlagArray[EventFlags_TailsUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_KnucklesAdventureComplete] = true;
-					EventFlagArray[EventFlags_AmyUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_BigUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_GammaUnlockedAdventure] = true;
-					break;
-				case 3:
-					EventFlagArray[EventFlags_TailsUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_KnucklesUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_AmyAdventureComplete] = true;
-					EventFlagArray[EventFlags_BigUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_GammaUnlockedAdventure] = true;
-					break;
-				case 4:
-					EventFlagArray[EventFlags_TailsUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_KnucklesUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_AmyUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_BigAdventureComplete] = true;
-					EventFlagArray[EventFlags_GammaUnlockedAdventure] = true;
-					break;
-				case 5:
-					EventFlagArray[EventFlags_TailsUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_KnucklesUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_AmyUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_BigUnlockedAdventure] = true;
-					EventFlagArray[EventFlags_GammaAdventureComplete] = true;
-					EventFlagArray[EventFlags_SuperSonicUnlockedAdventure] = true;
-					break;
-				case 6:
-					EventFlagArray[EventFlags_SuperSonicAdventureComplete] = true;
-					break;
-				}
-					GameMode = GameModes_StartCredits;
-					GameState = 21;
-					Credits_State = 1;
-					Load_SEGALOGO_E();
-			}
-
-			else
-			{
-				switch (CurrentLevel)
-				{
-				case LevelIDs_EmeraldCoast:
-					if (CurrentCharacter == Characters_Sonic || MetalSonicFlag == 1)
-					{
-						CurrentAct = 2;
-						NextAct = 2;
-					}
-					else
-					{
-						if (CurrentCharacter == Characters_Big)
-						{
-							CurrentAct = 0;
-							NextAct = 0;
-						}
-						else
-						{
-							NextAct = actHS[rand() % 2];
-							CurrentAct = actHS[rand() % 2];
-						}
-					}
-					break;
-				case LevelIDs_IceCap:
-					if (CurrentCharacter == Characters_Sonic)
-					{
-						NextAct = 3;
-						CurrentAct = 3;
-					}
-					else
-						if (CurrentCharacter == Characters_Big)
-						{
-							NextAct = 0;
-							CurrentAct = 0;
-						}
-						else
-						{
-							NextAct = actIC[rand() % 2];
-							CurrentAct = actIC[rand() % 2];
-						}
-					break;
-				case LevelIDs_HotShelter:
-					NextAct = actHS[rand() % 2];
-					CurrentAct = actHS[rand() % 2];
-					break;
-				case LevelIDs_SpeedHighway:
-					if (CurrentCharacter == Characters_Sonic)
-					{
-						CurrentAct = 0;
-					}
-					else
-					{
-						NextAct = actrng[rand() % 2];
-						CurrentAct = actrng[rand() % 2];
-					}
-					break;
-				case LevelIDs_RedMountain:
-					if (CurrentCharacter == Characters_Sonic)
-					{
-						CurrentAct = 0;
-					}
-					else
-					{
-						NextAct = actrng[rand() % 2];
-						CurrentAct = actrng[rand() % 2];
-					}
-					break;
-				}
+				EventFlagArray[EventFlags_SonicAdventureComplete] = true;
+				EventFlagArray[EventFlags_TailsUnlockedAdventure] = true;
+				EventFlagArray[EventFlags_KnucklesUnlockedAdventure] = true;
+				EventFlagArray[EventFlags_AmyUnlockedAdventure] = true;
+				EventFlagArray[EventFlags_BigUnlockedAdventure] = true;
+				EventFlagArray[EventFlags_GammaUnlockedAdventure] = true;
+				GameMode = GameModes_StartCredits;
+				GameState = 21;
+				Credits_State = 1;
+				CurrentCharacter = 0;
+				Load_SEGALOGO_E();
 			}
 		}
 
-		if (GameMode == 4 || GameMode == 5)
 		{
-			HudShowTimer;
-			HudDisplayRingTimeLife;
-			HudDisplayScoreOrTimer;
-		}
-
-		// Increase their MaxAccel to 5 so they can complete stages they are not meant to.
-		{
+			// Increase their MaxAccel to 5 so they can complete stages they are not meant to.
 			PhysicsArray[Characters_Amy].MaxAccel = 5;
 			PhysicsArray[Characters_Big].MaxAccel = 5;
 			PhysicsArray[Characters_Gamma].MaxAccel = 5;
 			return;
 		}
 
+
+		
 	}
 
 	__declspec(dllexport) void __cdecl OnControl()
 	{
 		
-		//fix Casinopolis SFX when using wrong characters
 		switch (CurrentLevel)
 		{
+			//fix Casinopolis SFX when using wrong characters
 		case LevelIDs_Casinopolis:
 			if (CurrentCharacter == Characters_Amy)
 			{
@@ -278,15 +150,17 @@ extern "C"
 				break;
 			}
 
+			//Fix Big Hot Shelter act 2 song
 			switch (CurrentLevel)
 			{
 			case LevelIDs_HotShelter:
 				if (CurrentCharacter == Characters_Big && CurrentAct == 1)
 				{
-					PlayMusic(MusicIDs_HotShelterRedBarrageArea);
+					CurrentSong = 80;
 				}
 				break;
 			}
+		
 	}
 
 	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
